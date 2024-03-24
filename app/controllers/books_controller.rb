1f50ee309f2af90ba6c6b3bac382b6a5a82212fe
EC2 Default User <ec2-user@ip-172-31-21-201.ap-northeast-1.compute.internal>
2024-03-24 05:21:27 +0000

    [Finish]CRUD implementation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,26 +24,24 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       flash.now[:alert] = "Error creating the book."
-
-      render :index
+      render :edit
     end
-  end
-
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to 'books'
+    redirect_to '/books'
   end
 
   private
